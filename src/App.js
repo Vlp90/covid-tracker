415d0da79aad1b13d88e758ddba44bf6ddd7b087
Vlp90 <vladimir.leloup@gmail.com>
2020-09-10 10:23:30 +0200

    form countrol useState value

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+import { FormControl, Select, MenuItem } from "@material-ui/core";
 
 function App() {
+  const [coutries, setCountries] = useState(["USA", "UK", "INDIA"]);
+
+  // the state is how to write a variable in react
+
   return (
     <div className="app">
-      <h1>COVID-19 tracker</h1>
+      <div className="app__header">
+        <h1>COVID-19 tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {coutries.map((country) => (
+              <MenuItem value={country}>{country}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
