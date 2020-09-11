38e2dd03368cfa6c0c0d11b5ff8196563165309e
Vlp90 <vladimir.leloup@gmail.com>
2020-09-11 12:18:02 +0200

    when cards clicked, circle on map are refreshed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import InfoBox from "./InfoBox";
 import Map from "./Map";
 import Table from "./Table";
-import { sortData } from "./util.js";
+import { sortData, prettyPrintStat } from "./util.js";
 import LineGraph from "./LineGraph.js";
 
 import "leaflet/dist/leaflet.css";
@@ -27,6 +27,7 @@ function App() {
   });
   const [mapZoom, setMapZoom] = useState(3);
   const [mapCountries, setMapCountries] = useState([]);
+  const [casesType, setCasesType] = useState("cases");
 
   // to make sure it will work with worldwide data at launch
   useEffect(() => {
@@ -62,7 +63,7 @@ function App() {
           const sortedData = sortData(data);
           // setTableData(data);
           setTableData(sortedData);
-          setMapCountries(data)
+          setMapCountries(data);
           setCountries(countries);
         });
     };
@@ -114,24 +115,27 @@ function App() {
 
         <div className="app__stats">
           <InfoBox
+            onClick={(e) => setCasesType("cases")}
             title="Coronavirus Cases"
-            cases={countryInfo.todayCases}
-            total={countryInfo.cases}
+            cases={prettyPrintStat(countryInfo.todayCases)}
+            total={prettyPrintStat(countryInfo.cases)}
           />
           <InfoBox
+            onClick={(e) => setCasesType("recovered")}
             title="Recovered"
-            cases={countryInfo.todayRecovered}
-            total={countryInfo.recovered}
+            cases={prettyPrintStat(countryInfo.todayRecovered)}
+            total={prettyPrintStat(countryInfo.recovered)}
           />
           <InfoBox
+            onClick={(e) => setCasesType("deaths")}
             title="Deaths"
-            cases={countryInfo.todayDeaths}
-            total={countryInfo.deaths}
+            cases={prettyPrintStat(countryInfo.todayDeaths)}
+            total={prettyPrintStat(countryInfo.deaths)}
           />
         </div>
 
         <div className="app__map">
-          <Map countries={mapCountries} center={mapCenter} zoom={mapZoom} />
+          <Map casesType={casesType} countries={mapCountries} center={mapCenter} zoom={mapZoom} />
         </div>
       </div>
 
--- src/util.js
+++ src/util.js
@@ -2,6 +2,16 @@ import React from "react";
 import { Circle, Popup } from "react-leaflet";
 import numeral from "numeral";
 
+// sort list of the countries
+export const sortData = (data) => {
+  const sortedData = [...data];
+
+  return sortedData.sort((a, b) => (a.cases > b.cases ? -1 : 1));
+};
+
+export const prettyPrintStat = (stat) =>
+  stat ? `+${numeral(stat).format("0.0a")}` : "+0";
+
 // Draw circles on the map
 const casesTypeColors = {
   cases: {
@@ -10,7 +20,7 @@ const casesTypeColors = {
     // half_op: "rgba(204,16,52,0.5)",
     multiplier: 800,
   },
-  recovored: {
+  recovered: {
     hex: "#7dd71d",
     // rgb: "rgb(125,215,29)",
     // half_op: "rgba(125,215,29,0.5)",
@@ -23,25 +33,34 @@ const casesTypeColors = {
     multiplier: 2000,
   },
 };
-// sort list of the countries
-export const sortData = (data) => {
-  const sortedData = [...data];
-
-  return sortedData.sort((a, b) => (a.cases > b.cases ? -1 : 1));
-};
-
-
 export const showDataOnMap = (data, casesType = "cases") =>
   data.map((country) => (
     <Circle
-    center={[country.countryInfo.lat, country.countryInfo.long]}
-    color={casesTypeColors[casesType].hex}
-    fillColor={casesTypeColors[casesType].hex}
-    fillOpacity={0.4}
-    radius={
-      Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
-    }
+      center={[country.countryInfo.lat, country.countryInfo.long]}
+      color={casesTypeColors[casesType].hex}
+      fillColor={casesTypeColors[casesType].hex}
+      fillOpacity={0.4}
+      radius={
+        Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
+      }
     >
-      <Popup>I am a popup</Popup>
+      <Popup>
+        <div className="info-container">
+          <div
+            className="info-flag"
+            style={{ backgroundImage: `url(${country.countryInfo.flag})` }}
+          ></div>
+          <div className="info-name">{country.country}</div>
+          <div className="info-confirmed">
+            Cases: {numeral(country.cases).format("0,0")}
+          </div>
+          <div className="info-recovered">
+            Recovered: {numeral(country.recovered).format("0,0")}
+          </div>
+          <div className="info-deaths">
+            Deaths: {numeral(country.deaths).format("0,0")}
+          </div>
+        </div>
+      </Popup>
     </Circle>
   ));
